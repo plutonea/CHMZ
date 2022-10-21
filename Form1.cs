2674d1a5c24ef2108f2f0e97cb7b537562547345
Admin <Admin@SALES-14>
2022-10-22 02:09:16 +0500

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CHMZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection conn;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connStr = "server=caseum.ru;port=33333;user=st_2_20_5;database=db_test;password=58224653;";
+            conn = new MySqlConnection(connStr);
+        }
+    }
+}
